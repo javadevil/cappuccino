96e093896637b27f0898344c0f604a70ae1229b5
javadevil <wt@Wacharabuhms-MacBook-Pro.local>
2015-09-15 15:17:19 +0700

    Change to angular material

---
+++ cappuccino-art/apps/main/index.js
@@ -0,0 +1,9 @@
+var app = angular.module('StarterApp', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+  $scope.mydate = new Date();
+ 
+}]);
--- cappuccino-art/index.js
+++ cappuccino-art/index.js
@@ -1,9 +1,9 @@
 var router = require('express').Router();
 var serveStatic = require('serve-static');
 
-module.exports = CappuccinoArt;
-function CappuccinoArt () {
-	router.use('/',serveStatic(__dirname+'/app'))
+module.exports = ServeStatic;
+function ServeStatic () {
+	router.use('/',serveStatic(__dirname+'/apps'));
 	router.use('/bower_components',serveStatic(__dirname+'/bower_components'));
 	return router;
 }
